32fba531d485c806fb4282aba979751b3b09c9bd
Borna <borna.grilec@speck.agency>
2020-06-22 12:00:13 +0200

    Added task description

--- one/src/components/Programming/Programming.js
+++ one/src/components/Programming/Programming.js
@@ -1,3 +1,11 @@
+// Add the missing code instead of /*...*/.
+// This component receives a list of programming languages as a prop `progLangs`.
+// `progLangs` is an array of objects.
+// Each object has an id and a name { id: 1, name: 'JavaScript' }
+// Show the first five programming languages
+// by passing the `name` prop to the <ProgLang /> component
+// Be careful not to have any warnings
+
 import React from 'react';
 import ProgLang from '../ProgLang/ProgLang';
 
